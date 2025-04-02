6c00bdef45ae1e08b9ae8e4849028453a7abca8c
YukselG PC <Guray95@hotmail.com>
2025-04-02 23:21:32 +0200

    refactor code. Delete unnecessary variables. Move listener for cell outside of loop, and deletage to sketchpad.

--- javascript.js
+++ javascript.js
@@ -3,44 +3,36 @@ const inputGridSize = document.querySelector("#grid-size");
 const generateGridButton = document.querySelector("button");
 let inputFavColor = document.querySelector("#favcolor");
 
-// the total width sizes of the "sketch pad" in pixels
-let totalSketchPadSize = 800;
 let gridSize = parseInt(inputGridSize.value, 10);
 
 // function to create grid: takes in gridsize, and computes a row with 'gridSize' cells, 'gridSize' times.
 function createGrid(gridSizeInput) {
-	let gridSize = gridSizeInput;
-	let totalCellSize = totalSketchPadSize / gridSize;
-
 	// clear grid
 	sketchpad.innerHTML = "";
 
-	// subtracting the thin border (2px in total);
-	let paddingSize = totalCellSize - 2;
-
 	// first for loop: x-axis
-	for (let rowIndex = 0; rowIndex < gridSize; rowIndex++) {
+	for (let rowIndex = 0; rowIndex < gridSizeInput; rowIndex++) {
 		const row = document.createElement("div");
 		row.classList.add("row");
 		sketchpad.appendChild(row);
 
 		// second for loop: y-axis
-		for (let cellIndex = 0; cellIndex < gridSize; cellIndex++) {
+		for (let cellIndex = 0; cellIndex < gridSizeInput; cellIndex++) {
 			const cell = document.createElement("div");
 			cell.classList.add("cell");
 			row.appendChild(cell);
-
-			cell.addEventListener("mouseover", function () {
-				cell.style.backgroundColor = "red";
-				// for random colors
-				//cell.style.backgroundColor = `rgb(${Math.random() * 255}, ${Math.random() * 255}, ${Math.random() * 255})`;
-			});
 		}
 	}
 }
 
-// create grid with default size of 16
-createGrid(gridSize);
+// delegate listener to sketchpad
+sketchpad.addEventListener("mouseover", (event) => {
+	if (event.target.classList.contains("cell")) {
+		event.target.style.backgroundColor = "red";
+		// for random colors
+		//event.target.style.backgroundColor = `rgb(${Math.random() * 255}, ${Math.random() * 255}, ${Math.random() * 255})`;
+	}
+});
 
 // make user choose grid size - listening for blur so the field is updated when losing focus
 inputGridSize.addEventListener("blur", (event) => {
@@ -64,6 +56,9 @@ generateGridButton.addEventListener("click", () => {
 	createGrid(gridSize);
 });
 
-//favColor.addEventListener();
+// create grid with default size of 16
+createGrid(gridSize);
 
+//favColor.addEventListener();
 // TODO: Get favorite color of user and use that when painting the grid
+// TODO: Implement rainbow colors functionality
